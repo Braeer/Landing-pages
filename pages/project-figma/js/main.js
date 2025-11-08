f2a6a4b1a30e10b529c1b5398a370bffacf2caa0
Timur <114247583+Braeer@users.noreply.github.com>
2025-11-09 02:24:54 +0300

    ADD: Landing 'Project-Figma'

---
+++ pages/project-figma/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var sitenav = document.querySelectorAll('.sidenav');
+  var instances__sitenav = M.Sidenav.init(sitenav);
+  var modal = document.querySelectorAll('.modal');
+  var instances__modal = M.Modal.init(modal);
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
